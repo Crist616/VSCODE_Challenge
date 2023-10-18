14aa2ecf1db048630b31ba88d8c254b738b4f31c
Crist616 <144681232+Crist616@users.noreply.github.com>
2023-10-18 22:28:37 +0100

    Add files via upload

---
+++ Challenge08.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#Cristiano Garcia
+#18/10/2023
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+#Print the fourth element of the list.
+print(numbers[3])
+
+#Print the sixth to the tenth element of the list.
+print(numbers[5:10])
+
+#Change the value of the seventh element to “onion”
+numbers[6] = "onion"
+
+#append
+numbers.append("banana")
+
+
+
+
+
+
+print(numbers)
